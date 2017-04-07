547e34259fc02d7afd618427d3fe80a9dc7215e3
Evette Cheung <FrancFranc@gmx.com>
2017-04-07 16:20:10 -0700

    Problem 3 solved

--- app.js
+++ app.js
@@ -54,14 +54,22 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sumAndMultiply(a,b,c){ //eslint-disable-line
 
+  var sumAndMultiplySUM  = sum(sum(a,b)[0], c)[0];
+  var sumAndMultiplyPRODUCT = multiply(multiply(a,b)[0], c)[0];
+  var sumAndMultiplyEQStringSUM = a + ' and ' + b + ' and ' + c + ' sum to ' + sumAndMultiplySUM + '.';
+  var sumAndMultiplyEQStringPRODUCT = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + sumAndMultiplyPRODUCT + '.';
+  return [sumAndMultiplySUM, sumAndMultiplyEQStringSUM, sumAndMultiplyEQStringPRODUCT, sumAndMultiplyPRODUCT];
 }
 
-// Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
-/////////////////////////////////////
+
+
+
+
+
 /* Problem 4
 Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
 "2,3,4 was passed in as an array of numbers, and 9 is their sum."
